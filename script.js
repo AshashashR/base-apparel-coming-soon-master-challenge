eab2358f41c901a00f584f2c4df4df61c76af857
RAKOTOSON Ashley <rakotoson@sincere-inc.co.jp>
2022-02-22 10:43:00 +0900

    updated script js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const form = document.getElementById("form");
-const email = document.getElementById("email");
+const form = document.getElementById('form');
+const email = document.getElementById('email');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   const emailVal = email.value;
 
-  if (validateEmail(email.value)) {
-    form.classList.remove("error");
+  if (!validateEmail(emailVal)) {
+    form.classList.add('error');
+    form.classList.remove('success');
   } else {
-    form.classList.add("error");
-    const small = form.querySelector("input-error");
-    small.innerText = "Please provide valid email";
+    form.classList.remove('error');
+    form.classList.add('success');
   }
 });
 
 function validateEmail(email) {
-  var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
